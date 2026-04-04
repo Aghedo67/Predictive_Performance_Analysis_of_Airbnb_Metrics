4ae11cdedf0f03269b61af81c6873d375f3fef09
Aghedo67 <113692288+Aghedo67@users.noreply.github.com>
2026-04-04 12:21:04 +0100

    Update suggestion.py

--- recommendation_system/suggestion.py
+++ recommendation_system/suggestion.py
@@ -138,7 +138,7 @@ def main():
         with col_left:
             st.subheader("⚠️ Technical Limitations")
             st.write(f"""
-            # - **Supervised Learning:** This model predicts sentiment based on a large labeled dataset.
+            #**Supervised Learning:** This model predicts sentiment based on a large labeled dataset.
             - **Positivity Bias:** Highly skewed data (0.8+) makes distinguishing high-quality stays a challenge.
             - **Cold Start:** The system requires existing review history to make accurate matches.
             """)
